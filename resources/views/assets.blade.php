53aec3dd46c81a0eb5ae30c1e1e2f660ee3b061d
Kevin Dierkx <kevin@distortedfusion.com>
2026-06-16 17:02:59 +0200

    Added color scheme hint for dark mode

--- resources/views/assets.blade.php
+++ resources/views/assets.blade.php
@@ -1,5 +1,6 @@
 <!-- DDFSN Styles -->
 <style>
+:root.dark {color-scheme: dark;}
 @foreach ($groupedColorVariables as $selector => $colorVariables)
 {{ $selector }} {@foreach ($colorVariables ?? [] as $colorVariableName => $colorVariableValue)--{{ $colorVariableName }}:{!! $colorVariableValue !!};@endforeach}
 @endforeach
